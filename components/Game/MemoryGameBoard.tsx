95a9c8ea0d915a07327e8675ba9c5abb9c94ea19
Matthew Yorkgitis <matt@itisconsultingllc.com>
2022-05-13 07:45:24 -0400

    Adding stubs for MemoryGameBoard and Controller

---
+++ components/Game/MemoryGameBoard.tsx
@@ -0,0 +1,14 @@
+export interface MemoryGameBoardProps {
+}
+
+const MemoryGameBoard = (props: MemoryGameBoardProps) => {
+
+    const {} = useMemoryGameController()
+
+    return (
+        <div>
+        </div>
+    )
+}
+
+export default MemoryGameBoard
---
+++ components/Game/useMemoryGameController.ts
@@ -0,0 +1,50 @@
+import {useEffect, useState} from "react";
+
+
+export type MemoryGameControllerConfig = {
+    // Must be an even number
+    boardSize: number
+}
+
+export type UseMemoryGameController = {
+
+}
+
+export type MemoryCard = {
+
+}
+
+const useMemoryGameController = ({ boardSize }: MemoryGameControllerConfig): UseMemoryGameController => {
+
+    const [cards, setCards] = useState([])
+
+    const [selectedCards, setSelectedCards] = useState([])
+
+    useEffect(() => {
+        // Perform logic initializing the game board
+        // Create cards and shuffle
+    }, [ boardSize ])
+
+    const handleSelected = (index: number) => {
+
+    }
+
+    const handleReset = () => {
+
+    }
+
+    const handleWin = () => {
+
+    }
+
+    const handleLose = () => {
+
+    }
+
+    return {
+        cards,
+        selectedCards,
+        handleSelected,
+        handleReset
+    }
+}
